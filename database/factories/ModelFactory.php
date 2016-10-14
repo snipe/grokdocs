a2af938a2bc96012ac95716f929eba784fa9eac8
snipe <snipe@snipe.net>
2016-10-14 14:34:28 -0700

    Added factories for user, project

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Project;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -12,13 +14,24 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'display_name' => $faker->name,
+        'username' => $faker->username,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Project::class, function (Faker\Generator $faker) {
+    
+    return [
+        'name' => $faker->name,
+        'summary' => $faker->sentence,
+    ];
+});
+
