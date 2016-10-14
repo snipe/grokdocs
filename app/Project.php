7d846464bede547da6d4fd2964c697ffbbffbc2d
snipe <snipe@snipe.net>
2016-10-14 16:18:30 -0700

    Project model

---
+++ app/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Project extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+
+
+}
