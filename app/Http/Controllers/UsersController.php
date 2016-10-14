f93b291321b232ab83486d247e0233af44b1a0e0
snipe <snipe@snipe.net>
2016-10-14 16:18:22 -0700

    Added users controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\User;
+
+class UsersController extends Controller
+{
+    
+    public function show($username) {
+
+        $user = User::findByUsername($username);
+        echo $user->username;
+        return view('users.show')->with('user', $user);
+
+    }
+
+}
